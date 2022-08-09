81a8672ded24fe7503161d5c699d5975548b11ef
lawlsnhfx <laurenwilson5@hotmail.com>
2022-08-09 01:14:21 +0000

    W6D1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,3 +3,14 @@ class Comment < ActiveRecord::Base
     belongs_to :user
     belongs_to :finstagram_post
 end
+
+class Comment < ActiveRecord::Base
+
+ 
+    belongs_to :user
+    belongs_to :finstagram_post
+  
+
+    validates_presence_of :text, :user, :finstagram_post
+  
+  end
